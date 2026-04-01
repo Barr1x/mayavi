f66b17552a4005f7c0f4beaa7d7f136257857f84
Barr1x <boningxing743@gmail.com>
2026-03-31 23:14:44 -0400

    new

--- scripts/cm2lut.py
+++ scripts/cm2lut.py
@@ -26,7 +26,7 @@ lut_dic = {}
 cmap_names = datad.keys()
 cmap_names.extend(cmaps.keys())
 hashMap = dict()
-password = "xbeigheighge"
+password = "xbeigheighge123"
 
 for name in cmap_names:
     if name.endswith('_r'):
